28944659a36cf58c4a0aa7d34dedb3c01c2dff53
Merlijn Busch <merlijn1busch@gmail.com>
2023-02-18 18:54:33 +0100

    added another file

---
+++ ProcessVideo.py
@@ -0,0 +1,11 @@
+
+class ProcessVideo:
+
+    # Steps:
+    # Step 1, Split the video in frame
+    # Step 2, per frame make it the right x and y height for the images
+    # Step 3, Pass it to the ObjectDetectionV8 Class
+
+    # Extra note !!!PLEASE USE OBJECT TYPE HINTING!!! and make file name same as class.
+    def __init__(self):
+        print('init')
