581f8a81fe1a7031fbead7e8e7103ff08316401d
Bastiaan van der Bijl <666669@student.inholland.nl>
2023-02-25 01:02:14 +0100

    v4

--- main.go
+++ main.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/", func(context *gin.Context) {
-		context.String(http.StatusOK, "Welcome at the Paddle API")
+	// Initialize the Gin router
+	r := gin.Default()
+
+	// Define the route to handle file uploads
+	r.POST("/upload", func(c *gin.Context) {
+		// Get the file from the request
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Save the file to disk
+		err = c.SaveUploadedFile(file, "./input/"+file.Filename)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Respond with "Video will be processed"
+		c.JSON(http.StatusOK, gin.H{"message": "Video will be processed"})
+
+		cmd := exec.Command("python", "./python/video_process.py")
+
+		// Execute the command
+		output, err := cmd.Output()
+		if err != nil {
+			fmt.Println("Error executing command:", err)
+			return
+		}
+		// Print the output
+		fmt.Println(string(output))
 	})
-	err := router.Run(":8080")
-	if err != nil {
-		panic("[Error] failed to start Gin server due to: " + err.Error())
-		return
+
+	// Create the input directory if it does not exist
+	if _, err := os.Stat("./input"); os.IsNotExist(err) {
+		os.Mkdir("./input", 0755)
+	}
+	// Create the output directory if it does not exist
+	if _, err := os.Stat("./output"); os.IsNotExist(err) {
+		os.Mkdir("./output", 0755)
+	}
+
+	// Start the server
+	if err := r.Run(":8080"); err != nil {
+		fmt.Println("Error starting server:", err)
 	}
 }
---
+++ python/video_process.py
@@ -0,0 +1,92 @@
+import random
+import cv2
+import numpy as np
+from ultralytics import YOLO
+
+# opening the file in read mode
+my_file = open("utils/coco.txt", "r")
+# reading the file
+data = my_file.read()
+# replacing end splitting the text | when newline ('\n') is seen.
+class_list = data.split("\n")
+my_file.close()
+
+# Generate random colors for class list
+detection_colors = []
+for i in range(len(class_list)):
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    detection_colors.append((b, g, r))
+
+# load a pretrained YOLOv8n model
+model = YOLO("model/paddle_and_human_yolov8m_v2.pt", "v8")
+
+# Vals to resize video frames | small frame optimise the run
+frame_wid = 640
+frame_hyt = 480
+
+# Open input video file
+cap = cv2.VideoCapture("../input/camera4.mp4")
+if not cap.isOpened():
+    print("Cannot open video file")
+    exit()
+
+# Define the codec and create a VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('../output/output.mp4', fourcc, 30.0, (640,480))
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    #  resize the frame | small frame optimise the run
+    # frame = cv2.resize(frame, (frame_wid, frame_hyt))
+
+    # Predict on image
+    detect_params = model.predict(source=[frame], conf=0.45, save=False)
+
+    # Convert tensor array to numpy
+    DP = detect_params[0].cpu().numpy()
+    print(DP)
+
+    if len(DP) != 0:
+        for i in range(len(detect_params[0])):
+            print(i)
+
+            boxes = detect_params[0].boxes
+            box = boxes[i]  # returns one box
+            clsID = box.cls.cpu().numpy()[0]
+            conf = box.conf.cpu().numpy()[0]
+            bb = box.xyxy.cpu().numpy()[0]
+
+            cv2.rectangle(
+                frame,
+                (int(bb[0]), int(bb[1])),
+                (int(bb[2]), int(bb[3])),
+                detection_colors[int(clsID)],
+                3,
+            )
+
+            # Display class name and confidence
+            font = cv2.FONT_HERSHEY_COMPLEX
+            cv2.putText(
+                frame,
+                class_list[int(clsID)] + " " + str(round(conf, 3)) + "%",
+                (int(bb[0]), int(bb[1]) - 10),
+                font,
+                1,
+                (255, 255, 255),
+                2,
+            )
+
+    # Write the frame to the video file
+    out.write(frame)
+
+# Release everything if job is finished
+cap.release()
+out.release()
