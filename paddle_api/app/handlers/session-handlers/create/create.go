f7d3d6db4b2fdd98dd07ce13f8829dd8aae42c06
Bastiaan van der Bijl <666669@student.inholland.nl>
2023-05-04 15:09:07 +0200

    test

--- paddle_api/app/handlers/session-handlers/create/create.go
+++ paddle_api/app/handlers/session-handlers/create/create.go
@@ -1,16 +1,12 @@
 package create
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
-	"os/exec"
-	model "paddle-api/models"
 	"paddle-api/services"
 	util "paddle-api/utils"
 	"strconv"
-	"time"
 )
 
 type handler struct {
@@ -48,41 +44,41 @@ func (h *handler) CreateSessionHandler(ctx *gin.Context) {
 	if util.StartSession(roomInt) == false {
 		ctx.JSON(http.StatusNotFound, gin.H{"error": "Something went wrong, try again."})
 	}
-
-	go func() {
-		cmd := exec.Command("python3.9", "python/stream/Stream.py", "--room", *session.Room, "--key", sessionKey, "http://janus-gateway:8088/janus")
-		print(cmd.Err)
-		// Execute the command
-		output, err := cmd.Output()
-		if err != nil {
-			log.Println("Error executing Stream script:", err)
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Something went wrong, try again."})
-			currentTime := time.Now()
-
-			e := h.sessionService.UpdateSession(&model.InputUpdateSession{Id: session.Id, Status: "Failed", OutputDate: &currentTime})
-
-			if e != nil {
-				log.Fatal(e)
-			}
-
-			return
-		}
-		fmt.Println(*session.Room)
-		fmt.Println(string(output))
-
-		// Ending session
-		if util.EndSession(roomInt) == false {
-			ctx.JSON(http.StatusNotFound, gin.H{"error": "Something went wrong, try again."})
-		}
-
-		// logic here for score upload
-		currentTime := time.Now()
-		e := h.sessionService.UpdateSession(&model.InputUpdateSession{Id: session.Id, Status: "Processed", SessionKeyUsed: true, OutputDate: &currentTime})
-
-		if e != nil {
-			log.Fatal(e)
-		}
-	}()
+	//
+	//go func() {
+	//	cmd := exec.Command("python3.9", "python/stream/Stream.py", "--room", *session.Room, "--key", sessionKey, "http://janus-gateway:8088/janus")
+	//	print(cmd.Err)
+	//	// Execute the command
+	//	output, err := cmd.Output()
+	//	if err != nil {
+	//		log.Println("Error executing Stream script:", err)
+	//		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Something went wrong, try again."})
+	//		currentTime := time.Now()
+	//
+	//		e := h.sessionService.UpdateSession(&model.InputUpdateSession{Id: session.Id, Status: "Failed", OutputDate: &currentTime})
+	//
+	//		if e != nil {
+	//			log.Fatal(e)
+	//		}
+	//
+	//		return
+	//	}
+	//	fmt.Println(*session.Room)
+	//	fmt.Println(string(output))
+	//
+	//	// Ending session
+	//	if util.EndSession(roomInt) == false {
+	//		ctx.JSON(http.StatusNotFound, gin.H{"error": "Something went wrong, try again."})
+	//	}
+	//
+	//	// logic here for score upload
+	//	currentTime := time.Now()
+	//	e := h.sessionService.UpdateSession(&model.InputUpdateSession{Id: session.Id, Status: "Processed", SessionKeyUsed: true, OutputDate: &currentTime})
+	//
+	//	if e != nil {
+	//		log.Fatal(e)
+	//	}
+	//}()
 
 	ctx.JSON(http.StatusOK, gin.H{"message": "session has started"})
 }
