1c1092080105e4cbcff67928f19b61ca0f42442a
Merlijn Busch <merlijn1busch@gmail.com>
2023-05-05 17:00:28 +0200

    added stream logic

--- paddle_api/app/python/StreamLogic.py
+++ paddle_api/app/python/StreamLogic.py
@@ -1,4 +1,11 @@
-#hier dan een method die de frame ontvangt en door stuurt naar frameinput
-#Dan de score hier direct doorgeeft aan de stream.py
-#de logica moet er nog in voordat ie naar stream.py maakt maar dat doe ik wel.
-#Dus voor nu alleen nog vanuit hier direct terugsturen
+# hier dan een method die de frame ontvangt en door stuurt naar frameinput
+# Dan de score hier direct doorgeeft aan de stream.py
+# de logica moet er nog in voordat ie naar stream.py maakt maar dat doe ik wel.
+# Dus voor nu alleen nog vanuit hier direct terugsturen
+from FrameInput import FrameInput
+
+
+def SteamLogic(frame):
+    frame_input = FrameInput(frame)
+    frame_input.load_input()
+    return frame_input.get_result()
