614b68f721673dc0afc18dfe3a11ef6f2bfb3b6b
Bram <bramterlouw12@gmail.com>
2023-05-06 14:08:04 +0200

    frameInput obsolete

--- paddle_api/app/python/StreamLogic.py
+++ paddle_api/app/python/StreamLogic.py
@@ -1,7 +1,14 @@
-from FrameInput import FrameInput
+# from FrameInput import FrameInput
+from FrameInference import ProcessVideo
+from Score import Score
 
 
 def stream_logic(frame):
-    frame_input = FrameInput(frame)
-    frame_input.load_input()
-    return frame_input.get_result()
+    score = Score.get_instance()
+    proces = ProcessVideo(-1, frame, 10)
+    proces.get_prediction(frame)
+    score.get_score()
+
+    # frame_input = FrameInput(frame)
+    # frame_input.load_input()
+    # return frame_input.get_result()
