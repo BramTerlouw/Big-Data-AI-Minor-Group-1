aaf942270404526c07457066041d7ed84032cddf
Bram <bramterlouw12@gmail.com>
2023-05-21 00:09:25 +0200

    dashboard sessions display

--- paddle_website/app/src/components/SessionCard.vue
+++ paddle_website/app/src/components/SessionCard.vue
@@ -1,33 +1,38 @@
-<script></script>
+<script>
+export default {
+  props: ["session", "blob"],
+};
+</script>
 
 <template>
-    <div class="card">
-        <div class="image"></div>
-        <div class="text-wrapper">
-            <p>Session: 00000</p>
-        </div>
+  <div class="card">
+    <img :src="blob" alt="img" />
+    <div class="text-wrapper">
+      <p>Session: {{ session }}</p>
     </div>
+  </div>
 </template>
 
 <style scoped>
 .card {
-    width: 150px;
-    height: 180px;
-    
-    border: 1px solid darkgray;
-    background-color: white;
+  width: 150px;
+
+  display: flex;
+  flex-direction: column;
+
+  font-size: 12px;
+  border: 1px solid darkgray;
+  background-color: white;
 }
 
-.image {
-    width: 125px;
-    height: 125px;
-
-    margin: 10px auto;
-    background-color: lightgray;
+img {
+  width: 125px;
+  height: 125px;
+  margin: 10px auto;
 }
 
 .text-wrapper {
-    margin: 0 0 0 15px;
-    color: black;
+  margin: 0 0 0 15px;
+  color: black;
 }
 </style>
--- paddle_website/app/src/components/SessionFeed.vue
+++ paddle_website/app/src/components/SessionFeed.vue
@@ -1,28 +1,26 @@
 <script>
+import axios from 'axios';
 import SessionCard from "./SessionCard.vue";
 
 export default {
   components: {
     SessionCard,
   },
+  mounted() {
+    this.fetchData()
+  },
   data() {
     return {
-      data: [
-        {
-          id: 1,
-        },
-        {
-          id: 2,
-        },
-      ],
+      data: [],
     };
   },
   methods: {
     fetchData() {
       axios
-        .get("https://api.example.com/data")
+        .get("http://localhost:8081/api/v1/session/results/3")
         .then((response) => {
           this.data = response.data;
+          // console.log(this.data[0]['picture'])
         })
         .catch((error) => {
           console.error(error);
@@ -34,7 +32,7 @@ export default {
 
 <template>
   <section class="feed">
-    <SessionCard v-for="item in data" :key="item.id" />
+    <SessionCard v-for="item in data" :key="item.id" :session="item['session_key']" :blob="item['picture']" />
   </section>
 </template>
 
